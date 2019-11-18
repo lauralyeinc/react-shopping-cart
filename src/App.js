dfacde974d026c144f8e6c7e4d1098491b064ea3
Laura <theimer.laura@gmail.com>
2019-11-18 14:04:36 -0500

    Step 2 done.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 import { createContext } from 'jest-runtime';
+import CartContext from "./contexts/CartContext"; 
 
 function App() {
 	const [products] = useState(data);

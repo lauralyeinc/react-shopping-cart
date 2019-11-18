fa0cb9f78c5c6c7b005892a9c6b19ba55674543b
Laura <theimer.laura@gmail.com>
2019-11-18 14:01:16 -0500

    Step one done, finished logic to addItem function.  Set up nav.

--- src/App.js
+++ src/App.js
@@ -6,36 +6,26 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import { createContext } from 'jest-runtime';
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([...cart, item]);
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
+		<ProductContext.Provider value={{products, addItem}}>
+			<CartContext.Provider value={ cart } >
+			<div className="App">
+				<Navigation />
+				<Route exact path="/" component={Products} />
+				<Route path="/cart" component={ShoppingCart} />
+			</div> 
+			</CartContext.Provider>
+		</ProductContext.Provider> 
 	);
 }
 
